84a861d794f4e65fded434fe86fe22b5b9923e7a
andyruddh <andyruddh@users.noreply.github.com>
2025-05-12 15:30:55 +0000

    Deploying to gh-pages from @ andyruddh/andyruddh.github.io@a3885dd61fb645d4b92f4fc5b831c34d7a15f638 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "projects-the-docks",
+        },{id: "post-a-post-with-plotly-js",
+        
+          title: "a post with plotly.js",
+        
+        description: "this is what included plotly.js code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/sample-posts/2025/03/26/plotly.html";
+          
+        },
+      },{id: "projects-the-docks",
           title: 'The Docks',
           description: "EP on Recorded Things",
           section: "Projects",handler: () => {
